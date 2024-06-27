caddbff3c57b4051e7c419d7f89924353038795a
Toshiyuki Masui <masui@pitecan.com>
2024-06-27 09:32:13 +0900

    上下キーも利用

--- rotation.js
+++ rotation.js
@@ -2,25 +2,21 @@
 // 回転ダイヤルとかの操作速度で動きを変える
 //
 
-const RIGHT = 0
-const LEFT = 1
-var direction = RIGHT
+const DIR = { RIGHT:0, LEFT:1 }
+var direction = DIR.RIGHT
 
-const RESETSTATE = 0 // リセット状態
-const CHECKSTATE = 1 // fast/slow判定前状態
-const SLOWSTATE = 2
-const FASTSTATE = 3
-var state = RESETSTATE;
+const STATE = { RESET:0, CHECK:1, SLOW:2, FAST:3 }
+var state = STATE.RESET;
 
 function slowState(){
-    if(state == CHECKSTATE){
+    if(state == STATE.CHECK){
 	// fast/slow 判断前だったとき
 	slow(direction)
     }
-    state = SLOWSTATE
+    state = STATE.SLOW
 }
 function resetState(){
-    state = RESETSTATE
+    state = STATE.RESET
 }
 var resetTimeout = null // 初期状態にもどる
 var slowModeTimeout = null // 速さチェック
@@ -30,12 +26,12 @@ var slowval = 0
 
 function fast(dir){
     direction = dir
-    fastval += (direction == RIGHT ? 1 : -1)
+    fastval += (direction == DIR.RIGHT ? 1 : -1)
     document.getElementById('fast').textContent = fastval;
 }
 function slow(dir){
     direction = dir
-    slowval += (direction == RIGHT ? 1 : -1)
+    slowval += (direction == DIR.RIGHT ? 1 : -1)
     document.getElementById('slow').textContent = slowval;
 }
 
@@ -43,19 +39,19 @@ function move(dir){
     direction = dir
     clearTimeout(slowModeTimeout)
     clearTimeout(resetTimeout)
-    if(state == RESETSTATE){
-	state = CHECKSTATE
+    if(state == STATE.RESET){
+	state = STATE.CHECK
 	slowModeTimeout = setTimeout(slowState,500)
     }
-    else if(state == CHECKSTATE){
-	state = FASTSTATE
+    else if(state == STATE.CHECK){
+	state = STATE.FAST
 	fast(dir)
 	fast(dir)
     }
-    else if(state == SLOWSTATE){ // 低速
+    else if(state == STATE.SLOW){ // 低速
 	slow(dir)
     }
-    else if(state == FASTSTATE){ // 高速
+    else if(state == STATE.FAST){ // 高速
 	fast(dir)
     }
     clearTimeout(resetTimeout)
@@ -63,11 +59,12 @@ function move(dir){
 }
 
 document.addEventListener('keydown', event => {
-    if(event.key == 'ArrowRight'){
-	move(RIGHT)
+    var key = event.key
+    if(key == 'ArrowRight' || key == 'ArrowUp'){
+	move(DIR.RIGHT)
     }
-    if(event.key == 'ArrowLeft'){
-	move(LEFT)
+    if(key == 'ArrowLeft' || key == 'ArrowDown'){
+	move(DIR.LEFT)
     }
 });
 
